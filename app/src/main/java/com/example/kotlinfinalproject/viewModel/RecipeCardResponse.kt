81ddfc5e3b53a474991655397b29fa32cfb337ae
julien <julien.riviere94120@gmail.com>
2024-02-02 22:43:11 +0100

    Changes by Chimo

---
+++ app/src/main/java/com/example/kotlinfinalproject/viewModel/RecipeCardResponse.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinfinalproject.viewModel
+
+import org.json.JSONObject
+import com.example.kotlinfinalproject.model.RecipeCard
+
+data class RecipeCardResponse(
+    val label: String,
+    val url: String,
+)
+{
+    
+    fun createRecipeCard(data: JSONObject): RecipeCard {
+        return RecipeCard(data.get("label") as String, data.get("images") as String);
+    }
+
+
+}
