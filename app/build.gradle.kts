7c1048c17ae8eaaf35e4f18beaf1ae09bbd7dda8
julien <julien.riviere94120@gmail.com>
2024-02-06 17:23:27 +0100

    ajout des dépendance necessaire a room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
 android {
     namespace = "com.example.kotlinfinalproject"
     compileSdk = 34
+    viewBinding {
+        enable = true
+    }
 
     buildFeatures {
         buildConfig = true
@@ -58,9 +61,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -73,7 +80,7 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
 
     // HTTP
-    implementation("com.google.code.gson:gson:2.8.9") // Serialization to and from JSON
+    implementation("com.google.code.gson:gson:2.10") // Serialization to and from JSON
 
     // Retrofit ==> HTTP client for Android
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -81,12 +88,12 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Viewmodel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
 
     // HTTP Client
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
     // Rx
     implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
@@ -98,11 +105,10 @@ dependencies {
 
     val room_version = "2.5.0"
 
-    implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
-
-    // To use Kotlin annotation processing tool (kapt)
-    kapt("androidx.room:room-compiler:$room_version")
+    // Room components
+    implementation ("androidx.room:room-runtime:$room_version")
+    kapt ("androidx.room:room-compiler:$room_version")
 
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
