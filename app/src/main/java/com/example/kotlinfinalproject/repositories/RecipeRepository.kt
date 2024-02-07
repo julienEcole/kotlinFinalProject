5c8dceabbe8b1b65a42ea7564c7d6c6dccb7d4b9
MarcMalha <marc.malha@beanstock.com>
2024-02-07 20:46:25 +0100

    Fix: remove unnecessary imports

--- app/src/main/java/com/example/kotlinfinalproject/repositories/RecipeRepository.kt
+++ app/src/main/java/com/example/kotlinfinalproject/repositories/RecipeRepository.kt
@@ -2,10 +2,8 @@ package com.example.kotlinfinalproject.repositories
 
 import com.example.kotlinfinalproject.model.RecipeCard
 import com.example.kotlinfinalproject.services.RecipeApiService
-import com.example.kotlinfinalproject.services.RecipeLocalService
 import com.example.kotlinfinalproject.services.enums.Type
 import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.schedulers.Schedulers
 
 class RecipeRepository(private val apiService: RecipeApiService) {
     fun getRandomRecipesAll(type: Type, random: Boolean, q: String): Flowable<List<RecipeCard>> {
