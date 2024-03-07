4229aa38c18b0e7d3b60dbb79546763634f30309
MarcMalha <marc.malha@beanstock.com>
2024-03-07 11:47:32 +0100

    Fix: observe UserDao query call on IO thread instead of main thread to not block app startup

--- app/src/main/java/com/example/kotlinfinalproject/viewModel/UserViewModel.kt
+++ app/src/main/java/com/example/kotlinfinalproject/viewModel/UserViewModel.kt
@@ -2,36 +2,39 @@ package com.example.kotlinfinalproject.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.kotlinfinalproject.user_model.UserData
+import com.example.kotlinfinalproject.model.User
 import com.example.kotlinfinalproject.repositories.UsersRepository
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import com.example.kotlinfinalproject.db.AppDatabase
-import com.example.kotlinfinalproject.db.provider.DatabaseProvider
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class UserViewModel(private val usersRepo: UsersRepository) : ViewModel() {
     private val disposeBag = CompositeDisposable()
-    val userLiveData: LiveData<UserData> = MutableLiveData()
+    val userLiveData: MutableLiveData<User> = MutableLiveData()
 
     init {
-        getOneRandomUser()
+        observeUserData()
     }
 
-    private fun getOneRandomUser() {
-        usersRepo.getRandomUser()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ user ->
-                (userLiveData as MutableLiveData).value = user
-            }, { error ->
-                Log.e("UserViewModel", "Error fetching user: ${error.message}")
-            })
-            .addTo(disposeBag)
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun observeUserData() {
+        GlobalScope.launch(Dispatchers.IO) {
+            usersRepo.getRandomUser()
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe({ user ->
+                    this@UserViewModel.userLiveData.postValue(user)
+                }, { error ->
+                    Log.e("UserViewModel", "Error fetching user: ${error.message}")
+                })
+                .addTo(disposeBag)
+        }
     }
 
     override fun onCleared() {
